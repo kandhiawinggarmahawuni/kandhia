91f92e44d22c328409f6df2aa49ce45f7ac6934a
kandhiawinggarmahawuni <97815729+kandhiawinggarmahawuni@users.noreply.github.com>
2022-01-15 23:59:23 -0800

    kandhia dan dhea
    kandhia dan dhea

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import "./Assets/App.scss"
+
+function App() {
+  return ( 
+    <div className="container">
+    <div className="header">header</div>
+
+  <div className="section">
+    <div className="Card Section-1">1</div>
+    <div className="Card Section-2">2</div>
+    <div className="Card Section-3">3</div>
+    <div className="Card Section-4">4</div>  
+    <div className="Card Section-5">5</div> 
+    <div className="Card Section-6">6</div>
+  </div>
+
+<div className="body">
+  <div className="body-Anak body-1">1</div>
+  <div className="body-Anak body-2">2</div>
+  <div className="body-Anak body-3">3</div>
+</div>
+
+
+</div>
+  );
+  
+}
+
+export default App;
